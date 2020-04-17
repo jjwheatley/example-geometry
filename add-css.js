780de5376d54513bc73d7425d1be3637cc639d43
john <john@jjwheatley.com>
2020-04-17 19:16:53 +0200

    Cube working

---
+++ add-css.js
@@ -0,0 +1,27 @@
+var sheet = (function() {
+	// Create the <style> tag
+	var style = document.createElement("style");
+
+	// Add a media (and/or media query) here if you'd like!
+	// style.setAttribute("media", "screen")
+	// style.setAttribute("media", "only screen and (max-width : 1024px)")
+
+	// WebKit hack :(
+	style.appendChild(document.createTextNode(""));
+
+	// Add the <style> element to the page
+	document.head.appendChild(style);
+
+	return style.sheet;
+})();
+
+function addCSSRule(selector, rules) {
+    let index = 0;
+    // console.log(sheet);
+	if("insertRule" in sheet) {
+		sheet.insertRule(selector + "{" + rules + "}", index);
+	}
+	else if("addRule" in sheet) {
+		sheet.addRule(selector, rules, index);
+	}
+}
